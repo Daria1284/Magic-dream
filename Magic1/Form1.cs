1c6dce4248b8150525dfde4d41b62da52c570194
Daria1284 <158023033+Daria1284@users.noreply.github.com>
2024-10-24 14:40:07 +0300

    close

--- Magic1/Form1.cs
+++ Magic1/Form1.cs
@@ -16,5 +16,10 @@ namespace Magic1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
